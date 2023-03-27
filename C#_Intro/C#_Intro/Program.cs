7f14efd91f033b925b05365d7332401f96e109d8
asgerbabayev <asgerbabayev@hotmail.com>
2023-03-27 18:53:06 +0400

    added c# introduction

---
+++ C#_Intro/C#_Intro/Program.cs
@@ -0,0 +1,86 @@
+﻿
+
+#region Control structures
+#region Variables
+//int age = 16;
+
+//age = 2;
+
+//string name = "16";
+
+//decimal pi = 3.14M;
+
+//var a = "dasdas";
+
+//dynamic d = true;
+
+//d = 5;
+
+//d = ""; 
+#endregion
+
+#region Loops
+//while ()
+//{
+
+//}
+
+
+//int sum = 0;
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(i);
+//    //sum += i;
+//}
+
+
+//int i = 0;
+
+//foreach (var item in Enumerable.Range(1, 10))
+//{
+//    Console.WriteLine(item);
+//}
+
+
+//for (; ; )
+//{
+
+//}
+
+//while (true)
+//{
+
+//}
+
+//do
+//{
+
+//} while (true);
+
+//while (i < 10)
+//{
+//    i++;
+//    if (i == 5)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(i);
+//}
+
+
+
+//do
+//{
+//    i++;
+//    Console.WriteLine(i);
+//} while (i < 10);
+
+//Console.WriteLine(sum);
+
+#endregion
+
+#endregion
+
+
+
+

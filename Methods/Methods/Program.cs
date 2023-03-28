beef39d826260415188dbe83d9eb154475305e6d
asgerbabayev <asgerbabayev@hotmail.com>
2023-03-28 20:13:52 +0400

    added methods

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Print();
+            //Print("", "");
+            //Test("");
+            //var result = Compare(4, 3);
+
+
+
+            //string month = Console.ReadLine();
+
+
+        }
+        //method signiture -> Method adı, parametr sayısı ve parametr tipləri ferqli olarsa metod overloading olmush olur.
+
+        //static void Print(string name)
+        //{
+        //    Console.WriteLine(name);
+        //}
+        //static void Print(string name, string surname)
+        //{
+        //    Console.WriteLine($"Name: {name} Surname: {surname}");
+        //}
+        //static void Print(string fulname, byte age)
+        //{
+        //    Console.WriteLine("Fullname: {0} Age: {1}", fulname, age);
+        //}
+
+        //Console.Write("Enter a number 1: ");
+        //int num1 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter a number 2: ");
+        //int num2 = int.Parse(Console.ReadLine());
+
+        //int res = Sum(num1, num2);
+        //Console.WriteLine(res);
+
+        //static int sum(int num1, int num2)
+        //{
+        //    return num1 + num2;
+        //}
+
+        //int result = SumOdd(1, 2, 22);
+
+        //static int SumOdd(params int[] arr)
+        //{
+        //    int sum = 0;
+        //    foreach (int item in arr)
+        //    {
+        //        if (item % 2 != 0)
+        //        {
+        //            sum += item;
+        //        }
+        //    }
+        //    return sum;
+        //}
+
+        //static int Compare(int a, int b)
+        //{
+        //    if (a > b)
+        //    {
+        //        return a;
+        //    }
+        //    return b;
+        //}
+
+        //static void Print()
+        //{
+        //    Console.WriteLine(Sum(1, 2));
+        //}
+
+        //static double Divide(double num1, double num2)
+        //{
+        //    return num1 / num2;
+        //}
+
+        //static void Print()
+        //{
+        //    Console.WriteLine(Sum(1, 2));
+        //}
+        //static void Test(params string[] arr)
+        //{
+        //    Console.WriteLine("Params metod");
+        //}
+        //static void Test(string str)
+        //{
+        //    Console.WriteLine("Normal metod");
+        //}
+        //static void Test(string str, string str2 = "")
+        //{
+        //    Console.WriteLine("Default metod");
+        //}
+    }
+}

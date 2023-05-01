f1fb6793a239584a58b17251514c50258a722a6e
asgerbabayev <asgerbabayev@hotmail.com>
2023-05-01 19:48:24 +0400

    Area CRUD

--- Asp.Net/BB204_Nest_Web_App/BB204_Nest_Web_App/Areas/NestAdmin/Controllers/CategoriesController.cs
+++ Asp.Net/BB204_Nest_Web_App/BB204_Nest_Web_App/Areas/NestAdmin/Controllers/CategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using BB204_Nest_Web_App.DAL;
+using BB204_Nest_Web_App.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 namespace BB204_Nest_Web_App.Areas.NestAdmin.Controllers
@@ -9,19 +11,76 @@ namespace BB204_Nest_Web_App.Areas.NestAdmin.Controllers
     {
         private readonly AppDbContext _context;
         public CategoriesController(AppDbContext context)
-        => _context = context;
-
-
+        {
+            _context = context;
+        }
         public async Task<IActionResult> Index()
         {
             return View(await _context.Categories.Where(x => x.IsDeleted == false).ToListAsync());
         }
-        public IActionResult Detail(int id)
+        public async Task<IActionResult> Detail(int id)
         {
-            return Json(new
+            return View(await _context.Categories.FirstOrDefaultAsync(x => x.Id == id));
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState["File"].ValidationState == ModelValidationState.Invalid) return View();
+
+            if (!category.File.ContentType.Contains("image/"))
             {
-                Id = id
-            });
+                ModelState.AddModelError("File", "File must be image format");
+                return View();
+            }
+            if (category.File.Length / 1024 > 200)
+            {
+                ModelState.AddModelError("File", "File must be less than 200kb");
+                return View();
+            }
+
+
+
+            category.Photo = category.File.FileName;
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await _context.Categories.FirstOrDefaultAsync(x => x.Id == id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Category category)
+        {
+            Category? exists = await _context.Categories.FirstOrDefaultAsync(x => x.Id == category.Id);
+            if (exists == null)
+            {
+                ModelState.AddModelError("", "Category is null");
+                return View();
+            }
+            exists.Name = category.Name;
+            exists.Logo = category.Logo;
+            exists.Photo = category.Photo;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            Category? exists = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (exists == null)
+            {
+                ModelState.AddModelError("", "Category is null");
+                return View();
+            }
+            //_context.Categories.Remove(exists);
+            exists.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
     }
 }
--- Asp.Net/BB204_Nest_Web_App/BB204_Nest_Web_App/Models/Category.cs
+++ Asp.Net/BB204_Nest_Web_App/BB204_Nest_Web_App/Models/Category.cs
@@ -1,11 +1,18 @@
-﻿namespace BB204_Nest_Web_App.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BB204_Nest_Web_App.Models;
 
 public class Category
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    //[Required]
+    [MaxLength(100), MinLength(2)]
+    public string Name { get; set; } = null!;
     public string Logo { get; set; }
-    public string Photo { get; set; }
+    public string Photo { get; set; } = null!;
+    [NotMapped]
+    public IFormFile File { get; set; } = null!;
     public bool IsDeleted { get; set; }
     public ICollection<Product> Products { get; set; }
 
